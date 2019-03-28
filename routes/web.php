f57dfb02e7d00e37240a6ca63a74a4f6508047cf
Michael Joyner <joyner.michael@gmail.com>
2019-03-28 08:41:07 +0800

    add redirects for old links

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
+Route::redirect('/homestay.html', 'https://villa.loherb.com.tw');
+Route::redirect('/economybed.html', 'https://villa.loherb.com.tw/rooms');
+Route::redirect('/luxurybed.html', 'https://villa.loherb.com.tw/rooms');
+Route::redirect('/vipbed.html', 'https://villa.loherb.com.tw/rooms');
+Route::redirect('/cafe.html', 'https://cuisine.loherb.com.tw');
+Route::redirect('/event.html', 'https://cuisine.loherb.com.tw/events');
+Route::redirect('/menu.html', 'https://cuisine.loherb.com.tw/menu');
+
+
+
 Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['localeSessionRedirect', 'localizationRedirect']], function() {
     Route::view('/', 'homepage');
 
