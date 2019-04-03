4de1cbe11ffbf2ff308ca76fd8bde23a99b1a7c9
Michael Joyner <joyner.michael@gmail.com>
2019-04-03 08:43:36 +0800

    wait for page load before init superhero

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,28 +3,31 @@ import Hammer from "hammerjs";
 
 window.superHero = new SuperHero();
 
-superHero.init()
-         .then(() => {
-             const hammertime = new Hammer(document.querySelector('.banner'));
-             hammertime.on('swipe', ev => {
-                 if(ev.deltaX > 20) {
-                     return superHero.showNext();
-                 }
 
-                 if(ev.deltaX < 20) {
-                     return superHero.showPrev();
-                 }
-             });
 
-             document.body.addEventListener('keyup', ({code}) => {
-                 if(code === 'ArrowLeft') {
-                     return superHero.showPrev();
-                 }
+window.addEventListener('load', () => {
+    superHero.init()
+             .then(() => {
+                 const hammertime = new Hammer(document.querySelector('.banner'));
+                 hammertime.on('swipe', ev => {
+                     if(ev.deltaX > 20) {
+                         return superHero.showNext();
+                     }
 
-                 if(code === 'ArrowRight') {
-                     return superHero.showNext();
-                 }
-             });
-         })
-    .catch(() => console.log('failed to initialise banner'));
+                     if(ev.deltaX < 20) {
+                         return superHero.showPrev();
+                     }
+                 });
 
+                 document.body.addEventListener('keyup', ({code}) => {
+                     if(code === 'ArrowLeft') {
+                         return superHero.showPrev();
+                     }
+
+                     if(code === 'ArrowRight') {
+                         return superHero.showNext();
+                     }
+                 });
+             })
+             .catch(() => console.log('failed to initialise banner'));
+});
